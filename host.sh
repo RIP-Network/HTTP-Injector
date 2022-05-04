e97165e7c9a9a1ab9da830160615927a3df9f01b
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2022-05-04 19:16:40 +0200

    Add files via upload

---
+++ host.sh
@@ -0,0 +1,198 @@
+#Dependencias
+
+dependencias() {
+
+        clear
+        printf "\e[1;93mConectando al HTTP\e[0m\n"
+        printf "\e[1;93mPor Favor espere\e[0m\n"
+        pkg upgrade && update 
+        pkg install python
+        pkg install python2 
+        pkg install python3 
+        printf "\e[1;93mConexion Aceptada \e[0m\n"
+        printf "\e[1;93mPor Favor Ejecute start.py\e[0m\n"
+
+
+}
+
+dependencias
+#Dependencias
+
+dependencias() {
+
+        clear
+        printf "\e[1;93mConectando al HTTP\e[0m\n"
+        printf "\e[1;93mPor Favor espere\e[0m\n"
+        pkg upgrade && update 
+        pkg install python
+        pkg install python2 
+        pkg install python3 
+        printf "\e[1;93mConexion Aceptada \e[0m\n"
+        printf "\e[1;93mPor Favor Ejecute start.py\e[0m\n"
+
+
+}
+
+dependencias
+#Dependencias
+
+dependencias() {
+
+        clear
+        printf "\e[1;93mConectando al HTTP\e[0m\n"
+        printf "\e[1;93mPor Favor espere\e[0m\n"
+        pkg upgrade && update 
+        pkg install python
+        pkg install python2 
+        pkg install python3 
+        printf "\e[1;93mConexion Aceptada \e[0m\n"
+        printf "\e[1;93mPor Favor Ejecute start.py\e[0m\n"
+
+
+}
+
+dependencias
+#Dependencias
+
+dependencias() {
+
+        clear
+        printf "\e[1;93mConectando al HTTP\e[0m\n"
+        printf "\e[1;93mPor Favor espere\e[0m\n"
+        pkg upgrade && update 
+        pkg install python
+        pkg install python2 
+        pkg install python3 
+        printf "\e[1;93mConexion Aceptada \e[0m\n"
+        printf "\e[1;93mPor Favor Ejecute start.py\e[0m\n"
+
+
+}
+
+dependencias
+#Dependencias
+
+dependencias() {
+
+        clear
+        printf "\e[1;93mConectando al HTTP\e[0m\n"
+        printf "\e[1;93mPor Favor espere\e[0m\n"
+        pkg upgrade && update 
+        pkg install python
+        pkg install python2 
+        pkg install python3 
+        printf "\e[1;93mConexion Aceptada \e[0m\n"
+        printf "\e[1;93mPor Favor Ejecute start.py\e[0m\n"
+
+
+}
+
+dependencias
+#Dependencias
+
+dependencias() {
+
+        clear
+        printf "\e[1;93mConectando al HTTP\e[0m\n"
+        printf "\e[1;93mPor Favor espere\e[0m\n"
+        pkg upgrade && update 
+        pkg install python
+        pkg install python2 
+        pkg install python3 
+        printf "\e[1;93mConexion Aceptada \e[0m\n"
+        printf "\e[1;93mPor Favor Ejecute start.py\e[0m\n"
+
+
+}
+
+dependencias
+#Dependencias
+
+dependencias() {
+
+        clear
+        printf "\e[1;93mConectando al HTTP\e[0m\n"
+        printf "\e[1;93mPor Favor espere\e[0m\n"
+        pkg upgrade && update 
+        pkg install python
+        pkg install python2 
+        pkg install python3 
+        printf "\e[1;93mConexion Aceptada \e[0m\n"
+        printf "\e[1;93mPor Favor Ejecute start.py\e[0m\n"
+
+
+}
+
+dependencias
+#Dependencias
+
+dependencias() {
+
+        clear
+        printf "\e[1;93mConectando al HTTP\e[0m\n"
+        printf "\e[1;93mPor Favor espere\e[0m\n"
+        pkg upgrade && update 
+        pkg install python
+        pkg install python2 
+        pkg install python3 
+        printf "\e[1;93mConexion Aceptada \e[0m\n"
+        printf "\e[1;93mPor Favor Ejecute start.py\e[0m\n"
+
+
+}
+
+dependencias
+#Dependencias
+
+dependencias() {
+
+        clear
+        printf "\e[1;93mConectando al HTTP\e[0m\n"
+        printf "\e[1;93mPor Favor espere\e[0m\n"
+        pkg upgrade && update 
+        pkg install python
+        pkg install python2 
+        pkg install python3 
+        printf "\e[1;93mConexion Aceptada \e[0m\n"
+        printf "\e[1;93mPor Favor Ejecute start.py\e[0m\n"
+
+
+}
+
+dependencias
+#Dependencias
+
+dependencias() {
+
+        clear
+        printf "\e[1;93mConectando al HTTP\e[0m\n"
+        printf "\e[1;93mPor Favor espere\e[0m\n"
+        pkg upgrade && update 
+        pkg install python
+        pkg install python2 
+        pkg install python3 
+        printf "\e[1;93mConexion Aceptada \e[0m\n"
+        printf "\e[1;93mPor Favor Ejecute start.py\e[0m\n"
+
+
+}
+
+dependencias
+#Dependencias
+
+dependencias() {
+
+        clear
+        printf "\e[1;93mConectando al HTTP\e[0m\n"
+        printf "\e[1;93mPor Favor espere\e[0m\n"
+        pkg upgrade && update 
+        pkg install python
+        pkg install python2 
+        pkg install python3 
+        printf "\e[1;93mConexion Aceptada \e[0m\n"
+        printf "\e[1;93mPor Favor Ejecute start.py\e[0m\n"
+
+
+}
+
+dependencias
